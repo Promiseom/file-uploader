fc8df3b3550c6e12d981d5a7ba4882f06f053248
Promise Anendah <anendahpromise@gmail.com>
2022-02-10 04:50:11 +0100

    Make server receive file binary data from client upload script, save to file and rename tmp file

--- upload.php
+++ upload.php
@@ -4,25 +4,62 @@
         session_start();
     }
     
-    // upload prepatation
-    if(strtolower($_SERVER["REQUEST_METHOD"]) == strtolower('POST')){        
+    // upload preparation
+    if(strtolower($_SERVER["REQUEST_METHOD"]) == strtolower('POST')){
+        // prepare to receive file binary payload data
         if(isset($_POST["fid"])){
             $filecode = $_POST["fid"];
-            $tempFilename = hash("sha512", $filecode.session_id());
-            print_r("tmp filename: $tempFilename");
-            $file = fopen("tmp/$tempFilename", "ab");
-            fclose($file);
+            $tempFilename = getTempFilename($filecode);
+            $responseData = ["filename"=> "tmp/$tempFilename", "newFile"=> true, "fileSize"=> 0];
+            // check if file exists, response will be the filesize
+            if(file_exists("tmp/$tempFilename")){
+                $responseData["newFile"] = false;
+                $responseData["fileSize"] = filesize("tmp/$tempFilename");
+            }else{
+                $file = fopen("tmp/$tempFilename", "ab");
+                fclose($file);
+            }
+            print_r(json_encode($responseData));
+            die();
         }else if(isset($_POST['cancel-upload'])){
             print_r("upload cancelled");
-        }else{
-            print_r("invalid data payload");
-        }
+            die();
+        }else if(isset($_POST["filedata"]) && isset($_POST["binData"])){
+            $fileMetadata = json_decode($_POST["filedata"]);
+            // files binary data is an array of binary data
+            $fileBinData = $_POST["binData"];
+            $bytesArray = mb_split(",", $fileBinData);
+            print_r("Number of bytes uploaded ".sizeof($bytesArray)." ");
+            $tmpFilename = "tmp/".getTempFilename($fileMetadata->fid);
+            if(file_exists($tmpFilename)){
+                $file = fopen($tmpFilename, "ab");
+                if($file != NULL){
+                    // append the binary content to the file
+                    $data = "";
+                    foreach($bytesArray as $byte){
+                        // we need %% since fprintf function uses % for string formatting
+                        $data .= pack("c*", $byte);
+                    }
+                    fprintf($file, "%s", $data);
+                    fclose($file);
+                    print_r("Done writing to file");
+                    // change file location when upload is complete
+                    if(filesize($tmpFilename) == $fileMetadata->fileSize){
+                        print_r("File upload complete, Renaming file");
+                        $result = completeFileUpload($fileMetadata->inputname, $tmpFilename, $fileMetadata->filename);
+                        print_r($result);
+                    }
+                }else{
+                    print_r("Server Error: Unable to save uploaded data segment.");
+                }
+            }else{
+                print_r("Invalid file meta data");
+            }
+            die();
+        }      
+    }
 
-        // recieve the request and save the file in the temporary file storage location
-        if(isset($_POST["upload-data-payload"]) && isset($_POST["upload-metadata"])){
-            $fileData = $_POST("upload-data-payload");
-            $metadata = $_POST("upload-metadata");
-        }
-        die();  
+    function getTempFilename($fileid){
+        return hash("sha512", $fileid.session_id()); 
     }
 ?>
